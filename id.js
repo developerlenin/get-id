e6e546ee17498b50f55a39a569bb278699b95bb4
developerlenin <justlenin9095@gmail.com>
2022-09-02 15:34:20 +0530

    new

---
+++ id.js
@@ -0,0 +1,9 @@
+   
+   
+     let input = document.getElementById("input-number");
+	   
+	  let button= document.getElementById("input-button");
+	  
+	   button.addEvenListener("click",  function() {
+			document.write(input.value)
+		});
