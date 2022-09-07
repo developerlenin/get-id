ab106bc2738011b8d1acca0769829d2a8ef1ede0
developerlenin <justlenin9095@gmail.com>
2022-09-07 18:22:04 +0530

    new

--- id.js
+++ id.js
@@ -3,7 +3,34 @@
      let input = document.getElementById("input-number");
 	   
 	  let button= document.getElementById("input-button");
+	   let button1= document.getElementById("input-button1");
 	  
-	   button.addEvenListener("click",  function() {
-			document.write(input.value)
-		});
+	  let screen1 =  document.getElementById("screen1");
+	  let screen =  document.getElementById("screen");
+	     
+	       let one = function(){
+			 let  p = document.createElement("p")
+			  
+			  p. innerText=input.value;
+			  screen.appendChild(p);
+			  input.value="";
+			   
+		   }
+	   
+	  button. addEventListener ("click" , function(){
+		  one();
+	  });
+	  
+	    let two = function(){
+			let p = document.createElement("p")
+			p.innerText=input.value;
+			screen1.appendChild(p);
+			input.value="";
+			
+		}
+	  
+	  button1. addEventListener("click", function(){
+		  two();
+	  });
+	  
+	  
