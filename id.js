cc9758adfe584e6c87449cbe1ffe769fe76d4c4e
developerlenin <justlenin9095@gmail.com>
2022-09-13 12:22:51 +0530

    new

--- id.js
+++ id.js
@@ -1,36 +1,42 @@
    
    
-     let input = document.getElementById("input-number");
-	   
-	  let button= document.getElementById("input-button");
-	   let button1= document.getElementById("input-button1");
-	  
-	  let screen1 =  document.getElementById("screen1");
-	  let screen =  document.getElementById("screen");
-	     
-	       let one = function(){
-			 let  p = document.createElement("p")
+    // var first = document.getElementById("box1");
+		 
+		 // var second = document.getElementById("box2");
+		 
+		 // var three = document.getElementById("paragrahp");
+		 
+		 // var four = document.getElementById("btn");
+		 
+	      // function add(){
 			  
-			  p. innerText=input.value;
-			  screen.appendChild(p);
-			  input.value="";
-			   
-		   }
-	   
-	  button. addEventListener ("click" , function(){
-		  one();
-	  });
-	  
-	    let two = function(){
-			let p = document.createElement("p")
-			p.innerText=input.value;
-			screen1.appendChild(p);
-			input.value="";
-			
-		}
-	  
-	  button1. addEventListener("click", function(){
-		  two();
-	  });
-	  
-	  
+		  // }
+		  
+		   function add(){
+			   box=parseIent(box.value);
+			    box1=parseIent(box1.value);
+				result=box+box1;
+				result.value=result;
+		   };
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
